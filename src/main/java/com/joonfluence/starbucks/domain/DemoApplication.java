d85c22b08a5edaca7cdc604e51fd15cc6d8fce48
joonho lee <joonfluence.dev@gmail.com>
2023-12-02 23:28:25 +0900

    [FIX] JwtAuthenticationFilter Post 요청 시, 권한 없음 반환

--- src/main/java/com/joonfluence/starbucks/domain/DemoApplication.java
+++ src/main/java/com/joonfluence/starbucks/domain/DemoApplication.java
@@ -1,9 +1,7 @@
-package com.joonfluence.starbucks;
+package com.joonfluence.starbucks.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class DemoApplication {
---
+++ src/main/java/com/joonfluence/starbucks/domain/user/auth/dto/request/RegisterRequest.java
@@ -0,0 +1,16 @@
+package com.joonfluence.starbucks.domain.user.auth.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegisterRequest {
+    private String name;
+    private String email;
+    private String password;
+}
