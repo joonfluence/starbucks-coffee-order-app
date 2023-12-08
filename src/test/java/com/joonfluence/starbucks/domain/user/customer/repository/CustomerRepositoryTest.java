16ccd75e76dc8e05a4102dd7eb005a6ee38d1218
joonho lee <joonfluence.dev@gmail.com>
2023-12-08 15:35:30 +0900

    fix(Auth) : Customer Repository 테스트

--- src/test/java/com/joonfluence/starbucks/domain/user/customer/repository/CustomerRepositoryTest.java
+++ src/test/java/com/joonfluence/starbucks/domain/user/customer/repository/CustomerRepositoryTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.dao.DataIntegrityViolationException;
-
 import java.util.NoSuchElementException;
 
 @DataJpaTest
